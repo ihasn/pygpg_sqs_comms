e3e7d900bd825e8056ee3f14c6d2ba06fe1a04db
ppierson <ppierson@bericotechnologies.com>
2014-08-03 19:06:27 -0400

    new file:   README.md 	new file:   sqs.py

---
+++ sqs.py
@@ -0,0 +1,38 @@
+import boto.sqs, gnupg, getpass
+import time
+from boto.sqs.message import Message
+aws_access_key_id = raw_input("AWS Access key: ")
+aws_secret_access_key = raw_input("AWS Secret key: ")
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+homedir_loc = raw_input('Type gpg home dir: ')
+gpg = gnupg.GPG(binary='/usr/bin/gpg2', homedir=homedir_loc)
+
+while True:
+    n = raw_input("Please enter option(send, recieve, quit): ")
+    if n.strip() == 'quit':
+        break
+    if n.strip() == 'send':
+        print gpg.list_keys()
+        key_id_queue = raw_input("GPG Key ID: ")
+        q = conn.create_queue(key_id_queue)
+        m = Message()
+        message = raw_input("Message: ")
+        encrypt_message = str(gpg.encrypt(message, key_id_queue))
+        print "Encrypted Message Send to SQS: " + encrypt_message
+        m.set_body(encrypt_message)
+        q.write(m)
+    if n.strip() == 'recieve':
+        print gpg.list_keys(True)
+        key_id_queue = raw_input("GPG Private ID: ")
+        password = getpass.getpass()
+        q = conn.get_queue(key_id_queue)
+        rs = q.get_messages()
+        m = rs[0]
+        pulled_encrypt_message = m.get_body()
+        print "Pulled Encrypted Message: " + pulled_encrypt_message
+        decrypted_message = str(gpg.decrypt(str(pulled_encrypt_message), passphrase=password))
+        print "Decrypted Message: " + decrypted_message
+
+
+#conn.delete_queue(q)
